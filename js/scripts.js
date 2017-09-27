e8812a7797ed8942a32887d90ad638305dfb877c
Alvin Niamatali and Hansen Hansen <hansen@>
2017-09-27 12:43:14 -0700

    adds 100 alert

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var element1 = function(currentScore){
   var dice = Math.floor((Math.random() * 6) + 1);
       if(dice < 2) {
         var currentScore = 0;
+        alert("END OF TURN");
         return currentScore;
       } else if(dice >= 2){
         currentScore += dice
@@ -24,17 +25,28 @@ $(document).ready(function() {
   $("button#roll").click(function(){
     if(index === 0){
       result1 = element1(result1);
-      $("ul#p1addition").append("<li>" + result1 + "</li>")
-      console.log(result1)
+      // console.log(result1);
+      $("ul#p1addition").append("<li>" + result1 + " </li>")
       if(result1 === 0){
         index = 1;
+      } else if((result1 + total1) >= 100) {
+        total1 += result1;
+        $("#result1").append("<li>" + total1 + " </li>");
+        alert("Player 1 Win!");
+        index = 2;
       }
     }
     else if(index === 1){
       result2 = element1(result2);
-      $("ul#p2addition").append("<li>" + result2 + "</li>")
+      // console.log(result2);
+      $("ul#p2addition").append("<li>" + result2 + " </li>")
       if(result2 === 0){
         index = 0;
+      } else if((result2 + total2) >= 100) {
+        total2 += result2;
+        $("#result2").append("<li>" + total2 + " </li>");
+        alert("Player 2 Win!");
+        index = 2;
       }
     }
   });
@@ -43,12 +55,12 @@ $(document).ready(function() {
     if (index === 1) {
       index = 0;
       total2 += result2;
-      $("#result2").text(total2);
+      $("#result2").append("<li>" + total2 + " </li>");
       result2 = 0;
     } else if (index === 0) {
       index = 1;
       total1 += result1;
-      $("#result1").text(total1);
+      $("#result1").append("<li>" + total1 + " </li>");
       result1 = 0;
     }
   });
